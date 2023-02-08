29b40f17864980fbd8bafcfc00f81eb4d39d4261
Joe Han <jhoon.han@outlook.com>
2023-02-07 20:15:06 -0500

    leggo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, useLocation } from "react-router-dom";
 
-function App() {
+import "./scss/App.scss";
+
+import Home from "./components/home/Home";
+
+const App = () => {
+  const location = useLocation();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact component={Home} />
+        </Switch>
       </header>
     </div>
   );
-}
+};
 
 export default App;
