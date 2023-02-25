4f5ce874e77c07713ec2c838e296842a81ae5913
Joe Han <jhoon.han@outlook.com>
2023-02-24 23:03:50 -0500

    fixing lambda

---
+++ cvtXlsxToJson/index.js
@@ -0,0 +1,52 @@
+const aws = require("aws-sdk");
+const XLSX = require("xlsx");
+const s3 = new aws.S3({ apiVersion: "2006-03-01" });
+
+const upload = async (bucket, key, convertedJson) => {
+  const newKey = key.replace(".xlsx", ".json");
+  const params = {
+    ACL: "public-read",
+    Body: convertedJson,
+    ContentType: "application/json",
+    Bucket: bucket,
+    Key: newKey,
+  };
+  return await new Promise((resolve, reject) => {
+    s3.putObject(params, (err, results) => {
+      if (err) reject(err);
+      else resolve(results);
+    });
+  });
+};
+
+const getFile = async (bucket, key) => {
+  const s3Object = await s3.getObject({ Bucket: bucket, Key: key }).promise();
+  const file = XLSX.read(s3Object.Body, { type: "buffer" });
+
+  return file;
+};
+
+const convert = (file) => {
+  const rowObject = XLSX.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
+  const convertedJson = JSON.stringify(rowObject);
+  return convertedJson;
+};
+
+exports.handler = async (event, context) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = event.Records[0].s3.object.key;
+  console.log(`#### BUCKET : ${bucket}`);
+  console.log(`#### KEY : ${key}`);
+
+  try {
+    const file = await getFile(bucket, key);
+    console.log(`#### FILE : ${file}`);
+    const convertedJson = convert(file);
+    return await upload(bucket, key, convertedJson);
+  } catch (error) {
+    console.error(error);
+    return { statusCode: 500, body: error.message };
+  }
+};
+
+exports.handler;
--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-const _AWS_URL =
-  "https://weddinginvitationapi-env.eba-r4xmx2pj.us-east-1.elasticbeanstalk.com";
+const _AWS_URL = "https://api.yeweon-junghoon.com";
 // const _AWS_URL = "http://localhost:3002"
 export const USER_URL = `${_AWS_URL}/user`;
 export const MESSAGE_URL = `${_AWS_URL}/message`;
