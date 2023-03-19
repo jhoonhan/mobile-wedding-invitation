bf03cfb9983f997f81dd52619851ac2dec5c353e
Joe Han <jhoon.han@outlook.com>
2023-03-18 21:11:22 -0400

    version added

--- src/config.js
+++ src/config.js
@@ -2,3 +2,4 @@ const _AWS_URL = "https://api.yeweon-junghoon.com";
 // const _AWS_URL = "http://localhost:3002"
 export const USER_URL = `${_AWS_URL}/user`;
 export const MESSAGE_URL = `${_AWS_URL}/message`;
+export const VERSION = "1.0.1";
